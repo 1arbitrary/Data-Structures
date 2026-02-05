1705e42205babe38b3dbd5458abfccd807119de7
dot <rawatkushagra33@gmail.com>
2026-02-05 18:07:24 +0530

    added Dynamic Sliding Window Implementation

--- Array/Sliding Window/Dynamic Window Implementation/main.cpp
+++ Array/Sliding Window/Dynamic Window Implementation/main.cpp
@@ -3,13 +3,25 @@
 int
 main (void)
 {
-  std::size_t arr[] = { 5, 1, 9, 2, 6, 3, 8, 4, 7 };
+  std::size_t arr[] = { 2, 1, 5, 2, 3, 2 };
   std::size_t length_arr = sizeof (arr) / sizeof (arr[0]);
+  std::size_t S = 7;
+  std::size_t left{ 0 };
+  std::size_t right{ 0 };
+  std::size_t accumulator = 0;
+  std::size_t minimum_valid_window_length = length_arr + 1;
 
-  std::size_t i{ 0 };
-  while (i < length_arr)
+  while (right < length_arr)
     {
-      std::cout << arr[i++] << "\t";
+      // Increase the window size by
+      accumulator += arr[right++];
+      while (S <= accumulator)
+        {
+          minimum_valid_window_length = right - left;
+          accumulator -= arr[left++];
+        }
     }
+
+  std::cout << minimum_valid_window_length;
   return 0;
 }
