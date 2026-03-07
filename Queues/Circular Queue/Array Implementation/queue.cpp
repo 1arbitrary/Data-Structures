4b48836baedb4747e499294f2b92c0889cb7ae2f
dot <rawatkushagra33@gmail.com>
2026-03-07 14:10:38 +0530

    added Circular Queue Implementation Using Array

--- Queues/Circular Queue/Array Implementation/queue.cpp
+++ Queues/Circular Queue/Array Implementation/queue.cpp
@@ -5,9 +5,9 @@ template <typename T> struct Queue
 {
   static constexpr std::size_t MAX_SIZE = 10;
   T elements[MAX_SIZE];
-  std::size_t occupied_indexes = 0;
-  int front = -1;
+  int front = 0;
   int rear = -1;
+  std::size_t occupied_indexes = 0;
 
   Queue () = default;
 
@@ -21,7 +21,7 @@ template <typename T> struct Queue
   }
 
   bool
-  is_empty ()
+  is_empty () const
   {
     return occupied_indexes == 0;
   }
@@ -31,13 +31,10 @@ template <typename T> struct Queue
   {
     if (is_full ())
       {
-        throw std::out_of_range ("Attempt To Insert On An Already Filled Queue !");
+        throw std::out_of_range ("Attempt To Enqueue On A Full Queue !");
       }
-    if (is_empty ())
-      {
-        front++;
-      }
-    elements[++rear] = data;
+    rear = (rear + 1) % MAX_SIZE;
+    elements[rear] = data;
     occupied_indexes++;
   }
 
@@ -48,8 +45,12 @@ template <typename T> struct Queue
       {
         throw std::out_of_range ("Attempt To Dequeue On An Empty Queue !");
       }
+
+    T deleted_element = elements[front];
+    front = (front + 1) % MAX_SIZE;
     occupied_indexes--;
-    return elements[front++];
+
+    return deleted_element;
   }
 
   T
@@ -57,8 +58,9 @@ template <typename T> struct Queue
   {
     if (is_empty ())
       {
-        throw std::out_of_range ("Attempt To Access The Front Of An Empty Queue !");
+        throw std::out_of_range ("Attempt To Access An Empty Front !");
       }
+
     return elements[front];
   }
 
@@ -68,14 +70,21 @@ template <typename T> struct Queue
     std::cout << "Queue : ";
     if (is_empty ())
       {
-        std::cerr << "Empty Queue !" << '\n';
+        std::cerr << "Empty Queue !";
         return;
       }
-    
-    int i{ 0 };
-    while (i <= rear)
+
+    int i{ front };
+    std::size_t elements_left_to_print{ occupied_indexes };
+
+    while (elements_left_to_print != 0)
       {
         std::cout << elements[i++] << '\t';
+        elements_left_to_print--;
+        if (i == MAX_SIZE)
+          {
+            i = 0;
+          }
       }
     std::cout << '\n';
   }
@@ -84,5 +93,23 @@ template <typename T> struct Queue
 int
 main (void)
 {
+  Queue<int> q;
+  q.enqueue (10);
+  q.enqueue (20);
+  q.enqueue (30);
+  q.enqueue (40);
+  std::cout << "Deleted : " << q.dequeue () << '\n';
+  std::cout << "Deleted : " << q.dequeue () << '\n';
+  q.enqueue (50);
+  q.enqueue (60);
+  q.enqueue (70);
+  q.enqueue (80);
+  q.enqueue (90);
+  q.dequeue ();
+  q.enqueue (100);
+  q.enqueue (110);
+  q.enqueue (120);
+  q.enqueue (140);
+  q.print ();
   return 0;
 }
